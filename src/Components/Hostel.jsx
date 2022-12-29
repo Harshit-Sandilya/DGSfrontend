cb58c91e2578905de0f0fbf8489078aafbc33b88
chandra-pro <goluchandra1812@gmail.com>
2022-12-29 20:12:22 +0530

    location award calender added

--- src/Components/Hostel.jsx
+++ src/Components/Hostel.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import hostel1 from '../Images/Hostel/hostel1.jpg';
+import hostel2 from '../Images/Hostel/hostel3.jpg';
+import hostel3 from '../Images/Hostel/hostel2.jpg';
+import hostel4 from '../Images/Hostel/hostel4.jpg';
+
+import '../Styles/Hostel.css';
 function Hostel() {
   return (
     <div className='Hostel'>
@@ -18,6 +23,43 @@ function Hostel() {
           <img src={hostel1} className="hostel1-img"></img>
         </div>
         </div>  
+        <div className='Mid-line'>
+
+        </div>
+     <div className='Hostel-footer'>
+      <div className='Hostel-mid-1'>
+        <div className='Hostel-mid-1-left'>
+          <img src={hostel2} className="hostel2-img"></img>
+        </div>
+        <div className='Hostel-mid-1-right'>
+
+        </div>
+      </div>
+      <div className='Hostel-mid-2'>
+        <div className='Hostel-mid-2-left'>
+
+        </div>
+        <div className='Hostel-mid-2-right'>
+          <img src={hostel3} className="hostel3-img"></img>
+        </div>
+      </div>
+      <div className='Hostel-mid-3'>
+        <div className='Hostel-mid-3-left'>
+          <img src={hostel4} className="hostel4-img"></img>
+        </div>
+    <div className='Hostel-mid-3-right'>
+          
+    </div>
+      </div>
+      <div className='Hostel-mid-4'>
+        <div className='Hostel-mid-4-left'>
+
+        </div>
+        <div className='Hostel-mid-4-right'>
+          <img src={hostel1} className="hostel5-img"></img>
+        </div>
+      </div>
+     </div>
     </div>
   )
 }
