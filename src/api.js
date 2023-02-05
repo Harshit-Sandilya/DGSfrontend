9f67f6b4013b5e49c95b13848bf5270b2e8483b3
RUPESH <89046490+rupesh031@users.noreply.github.com>
2023-02-05 11:35:31 +0530

    build home

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/";
+const API_URL = "https://dgsbackend.onrender.com/";
 
 function postTeacher(data) {
   console.log(data);
@@ -33,15 +33,15 @@ function postStudent(data) {
 }
 
 function postContact(data) {
-	console.log(data);
-	fetch(`${API_URL}contact`, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	})
-		.then((response) => response.json())
-		.then((value) => console.log(value))
-		.catch((err) => console.log(err));
+  console.log(data);
+  fetch(`${API_URL}contact`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((value) => console.log(value))
+    .catch((err) => console.log(err));
 }
 
 export { postTeacher, postStudent, postContact };
